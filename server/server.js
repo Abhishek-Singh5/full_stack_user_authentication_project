6e06fc59684349a383a61fcbc9d72b7fa0285a51
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-07-27 15:05:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173",  // or your frontend URL
+  origin: "*",  // or your frontend URL
   credentials: true
 }));
 
